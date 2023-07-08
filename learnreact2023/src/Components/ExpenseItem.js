48d96b7f3dd1baab1d589c1f7166dc52f530ff85
ZuhebAkhter <zuheb1998@gmail.com>
2023-07-09 00:22:08 +0530

    making simpler

--- learnreact2023/src/Components/ExpenseItem.js
+++ learnreact2023/src/Components/ExpenseItem.js
@@ -1,17 +1,15 @@
 import React from "react";
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 
 const ExpenseItem = (props) => {
  
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-        <div>{props.Location}</div>
-      </div>
+       <ExpenseDate date={props.date}></ExpenseDate>
+     <ExpenseDetails title={props.title} amount={props.amount} Location={props.Location}></ExpenseDetails>
     </div>
   );
 };
