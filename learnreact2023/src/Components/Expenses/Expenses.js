0749d8ec5bee400220d69dff14f9111e375dc91a
ZuhebAkhter <zuheb1998@gmail.com>
2023-07-12 12:12:53 +0530

    conditionalrendering

--- learnreact2023/src/Components/Expenses/Expenses.js
+++ learnreact2023/src/Components/Expenses/Expenses.js
@@ -1,13 +1,13 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
   const filteredExpenses = props.items.filter((expense) => {
@@ -16,17 +16,26 @@ const Expenses = (props) => {
 
   return (
     <Card className="expenses">
-              <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-        key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-     
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {/* {filteredExpenses.length===0 && <div><p>Only single Expense here. Please add more..." */}
+{/* </p></div>} */}
+{filteredExpenses.length === 1 && <p>Only single Expense here. Please add more...
+</p>  }
+      {filteredExpenses.length === 0 ? (
+        <p>No Expenses Found</p> 
+      ) : (
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      )}
     </Card>
   );
 };
